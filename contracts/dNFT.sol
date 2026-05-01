680cb1b49dbe4bcbad882d470df21628ed1113cc
HopeMabuza <londiwemabuza94@gmail.com>
2026-05-01 12:29:39 +0200

    contract is compiling

---
+++ contracts/dNFT.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+
+contract dNFT is Initializable, ERC721Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
+
+    uint256 public nextTokenId;
+    string public baseURI;
+    mapping(uint256 => uint256) public tokensSiteVisits;
+
+    uint256[50] private __gap;
+
+    event MintedNFT(uint256 tokenId, address owner);
+    event PointsUpdated(uint256 tokenId, uint256 points);
+    event UpdatedMetadata(uint256 tokenId);
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(string memory baseURI_) public initializer {
+        baseURI = baseURI_;
+        __ERC721_init("GROUP-dNFT", "DEVM");
+        __Ownable_init(msg.sender);
+    }
+
+    function mint() public payable {
+        require(balanceOf(msg.sender) == 0, "Already minted one NFT");
+        require(msg.value == 0.0001 ether, "Requires exactly 0.0001 ether");
+        uint256 tokenId = nextTokenId;
+        _safeMint(msg.sender, tokenId);
+        tokensSiteVisits[tokenId] = 0;
+        nextTokenId++;
+        emit MintedNFT(tokenId, msg.sender);
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        _requireOwned(tokenId); 
+        if (tokensSiteVisits[tokenId] < 15) {
+            return string(abi.encodePacked(baseURI, "0.json"));
+        } else {
+            return string(abi.encodePacked(baseURI, "1.json"));
+        }
+    }
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal virtual override returns (address)
+    {
+        address from = _ownerOf(tokenId);
+        if (from != address(0) && to != address(0)) {
+            revert("Soulbound: cannot transfer this NFT");
+        }
+        return super._update(to, tokenId, auth);
+    }
+
+    function setBaseURI(string memory baseURI_) public onlyOwner {
+        baseURI = baseURI_;
+    }
+
+    function updatePoints(uint256 tokenId, uint256 points) public onlyOwner {
+        _requireOwned(tokenId); 
+        tokensSiteVisits[tokenId] += points;
+        emit PointsUpdated(tokenId, tokensSiteVisits[tokenId]);
+        emit UpdatedMetadata(tokenId);
+    }
+
+    function _authorizeUpgrade(address newImpl) internal override onlyOwner {}
+}
